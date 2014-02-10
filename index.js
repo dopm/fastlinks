4260dc2183065b76c438a8df2f2c440e73df1bf4
Hsiaoming Yang <me@lepture.com>
2014-02-10 17:28:40 +0800

    Implement the basic workflow. It works basically now.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
+var delegate = require('delegate');
 var domparse = require('dom-parser');
+var execute = require('execute-script');
 var urllib = require('url');
 
 var isSupported = history && history.pushState && history.replaceState;
@@ -9,6 +11,7 @@ function Turbolinks() {
   this.pageCache = {};
   this.state = null;
   this.referrer = null;
+  this.transitionCache = false;
 
   if (isSupported) {
     history.replaceState({turbolinks: true, url: location.href}, '', location.href);
@@ -21,22 +24,85 @@ require('emitter')(Turbolinks.prototype);
 
 Turbolinks.prototype.visit = function(url) {
   if (!isSupported) return location.href = url;
-};
 
-Turbolinks.prototype.fetch = function(url) {
+  var me = this;
   // remember referer
-  this.referrer = document.location.href;
+  me.referrer = location.href;
 
   // cache current page
 
   // reflect new url
-  if (url !== this.referrer) {
+  if (url !== me.referrer) {
     history.pushState({turbolinks: true, url: url}, '', url);
   }
 
-  // fetch replacement
+  if (me.transitionCache) {
+    var cached = me.pageCache[url];
+    if (cached) return me.fetch(url);
+  }
+
+  me.fetch(url, function() {
+    if (location.hash) {
+      return location.href = location.href;
+    } else {
+      window.scrollTo(0, 0);
+    }
+  });
 };
 
+Turbolinks.prototype.fetch = function(url, cb) {
+  var me = this;
+  me.emit('page:fetch', {url: url});
+  me.request(url, function(xhr) {
+    var ct = xhr.getResponseHeader('Content-Type');
+    var doc;
+    if (validContentType(ct) && validStatus(xhr.status)) {
+      doc = domparse(xhr.responseText);
+    }
+    if (!doc) {
+      return location.href = url;
+    }
+
+    var data = extractTitleAndBody(doc);
+    data.runscript = true;
+    me.render(data);
+
+    // reflect redirected url
+    var loc = xhr.getResponseHeader('X-XHR-Redirected-To');
+    if (loc) {
+      // TODO: hash
+      history.replaceState(me.state, '', loc);
+    }
+
+    cb && cb();
+  });
+};
+
+/**
+ * Render data to document.
+ */
+Turbolinks.prototype.render = function(data) {
+  if (data.title) {
+    document.title = data.title;
+  }
+  document.documentElement.replaceChild(data.body, document.body);
+
+  if (data.head) {
+    updateHead(data.head);
+  }
+
+  if (data.runscript) {
+    executeScripts(document.body);
+  }
+
+  this.state = history.state;
+  this.emit('page:change');
+  this.emit('page:update');
+};
+
+/**
+ * Send xhr request.
+ */
 Turbolinks.prototype.request = function(url, cb) {
   var me = this;
 
@@ -49,11 +115,7 @@ Turbolinks.prototype.request = function(url, cb) {
 
   xhr.onload = function() {
     me.emit('page:receive');
-    var ct = xhr.getResponseHeader('Content-Type');
-    if (!ct.match(/^(?:text\/html|application\/xhtml\+xml|application\/xml)(?:'|$)/)) {
-      return location.href = url;
-    }
-    cb && cb(domparse(xhr.responseText));
+    cb && cb(xhr);
   };
 
   xhr.onerror = function() {
@@ -73,6 +135,56 @@ Turbolinks.prototype.request = function(url, cb) {
   return xhr;
 };
 
+
+function validContentType(ct) {
+  return ct.match(/^(?:text\/html|application\/xhtml\+xml|application\/xml)(?:;|$)/);
+}
+function validStatus(code) {
+  return code < 400;
+}
+
+function executeScripts(body) {
+  var scripts = body.querySelectorAll('script:not([data-turbolinks-eval="false"])');
+  for (var i = 0; i < scripts.length; i++) {
+    execute(scripts[i]);
+  }
+}
+
+function updateHead(head) {
+  var nodes = head.querySelectorAll('meta');
+  for (var i = 0; i < nodes.length; i++) {
+    (function(meta) {
+      if (!meta.name) return;
+      var selector = 'meta[name="' + meta.name + '"]';
+      var original = document.head.querySelector(selector);
+      if (original) original.content = meta.content;
+    })(nodes[i]);
+  }
+}
+
+function extractTitleAndBody(doc) {
+  var ret = {};
+
+  var title = doc.querySelector('title');
+  ret.title = title ? title.textContent: null;
+
+  var body = doc.body;
+  // remove <noscript>
+  body.innerHTML = body.innerHTML.replace(/<noscript[\S\s]*?<\/noscript>/ig, '');
+  ret.body = body;
+
+  return ret;
+}
+
 // exports API
-exports = module.exports = new Turbolinks();
-exports.Turbolinks = Turbolinks;
+var turbolinks = new Turbolinks();
+turbolinks.Turbolinks = Turbolinks;
+module.exports = turbolinks;
+
+if (isSupported) {
+  delegate.bind(document, 'a', 'click', function(e) {
+    // TODO: ignore click
+    turbolinks.visit(e.delegateTarget.href);
+    return e.preventDefault();
+  }, true);
+}
